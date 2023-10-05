716958e0d3f5534b315c1621ba9ae59421b44faa
IanCummings3 <25547367-IanCummings3@users.noreply.replit.com>
2023-10-05 21:58:35 +0000

    k

--- game_loop.py
+++ game_loop.py
@@ -2,14 +2,14 @@ import random
 
 
 def gameloop_1():
-  temp_value = random.randint(1,15)
-  if temp_value == 1:
-    print("you encountered a chest")
-  if temp_value == 2:
-    print("you encounter a town")
-  if temp_value == 3:
-    print("you encounter a camp of explorers")
-  else:
-    monster_encounter = ['goblin','goblin','skeleton','green_slime','green_slime']
-    print('you encountered a ' + random.choice(monster_encounter))
-    gameloop_1
+    temp_value = random.randint(1,15)
+    if temp_value == 1:
+      print("you encountered a chest")
+    elif temp_value == 2:
+      print("you encounter a town")
+    elif temp_value == 3:
+      print("you encounter a camp of explorers")
+    else:
+     monster_encounter = ['goblin','goblin','skeleton','green_slime','green_slime']
+     print('you encountered a ' + random.choice(monster_encounter))
+  gameloop_1
